fadd463c7f109693a3a534d041e697e86dcb65e3
Dulithi Amunugama <dulithi.a@gmail.com>
2025-07-03 10:57:32 +0530

    Improvments

--- Madlib.py
+++ Madlib.py
@@ -1,9 +1,22 @@
-name = input("Male name:")
-food = input("Food name:")
-drink = input("Drink name:")
-city = input("City name:")
+adjective = input("Adjective : ")
 
-print(f"{name} is a little boy. who dosen't like eat some kind of foods. Eventho there are soo many foods \
- he only eats {food} with {drink}. He lives in {city}.")
+name = input("Name : ")
+
+animal = input("Animal : ")
+
+verb_ing = input("Verb ending in -ing : ")
+
+food = input("Food (plural) : ")
+
+emotion = input("Emotion : ")
+
+place = input("Place : ")
+
+verb_past = input("Verb (past tense) : ")
+
+object = input("Object : ")
+
+sound = input("Sound : ")
 
 
+print(f"It was a {adjective} day when {name} decided to take their pet {animal} for a walk. While they were {verb_ing}, they stumbled upon a pile of {food}. Feeling {emotion}, they picked it all up and ran to the nearest {place}. Suddenly, the sky {verb_past}, and a giant {object} fell from the clouds with a loud \"{sound}\". Everyone screamed, but {name} just laughed and said, \"Another normal day!\"")
