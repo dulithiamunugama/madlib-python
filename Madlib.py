58f61f53d577e522e08cbd95b1b53e2dbc95c788
Dulithi Amunugama <dulithi.a@gmail.com>
2025-07-03 10:49:44 +0530

    Initial commit

---
+++ Madlib.py
@@ -0,0 +1,9 @@
+name = input("Male name:")
+food = input("Food name:")
+drink = input("Drink name:")
+city = input("City name:")
+
+print(f"{name} is a little boy. who dosen't like eat some kind of foods. Eventho there are soo many foods \
+ he only eats {food} with {drink}. He lives in {city}.")
+
+
